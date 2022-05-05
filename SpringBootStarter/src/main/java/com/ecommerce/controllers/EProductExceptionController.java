d2508289a280495338972701fc91c5ed925d1e41
Akhila Myana <myanaakhila23@gmail.com>
2022-05-05 23:17:25 +0530

    spring files added

---
+++ SpringBootStarter/src/main/java/com/ecommerce/controllers/EProductExceptionController.java
@@ -0,0 +1,18 @@
+package com.ecommerce.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.ecommerce.exceptions.ProductNotFoundException;
+
+@ControllerAdvice
+public class EProductExceptionController {
+
+        @ExceptionHandler(value = ProductNotFoundException.class)
+           public ResponseEntity<String> exception(ProductNotFoundException exception) {
+              return new ResponseEntity<String>("Product not found", HttpStatus.NOT_FOUND);
+           }
+}
+
